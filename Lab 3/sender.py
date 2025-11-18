a70d8e4194ecd75b050f1b78fb6c7eced288f0dc
Moebel <timo.moebel@epfl.ch>
2025-11-18 17:02:56 +0100

    sender

---
+++ Lab 3/sender.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python3.5
+
+import socket, struct, sys, time
+
+MCAST_GRP = ''
+MCAST_PORT = 0
+file = None
+interval = 1
+if len(sys.argv) != 4:
+    print('usage: %s <mcast_address> <port> <sciper>' % sys.argv[0])
+    sys.exit(1)
+
+try:
+    MCAST_GRP = sys.argv[1]
+    MCAST_PORT = int(sys.argv[2])
+    sciper = int(sys.argv[3])
+except:
+    print('Invalid parameters')
+    print('usage: %s <mcast_address> <port> <sciper>' % sys.argv[0])
+    sys.exit(2)
+    
+cl_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+ttl = struct.pack('b', 10)
+cl_sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
+
+while True:
+    try:
+        opinion = input()
+    except EOFError:
+            break     
+           
+    cl_sock.sendto((sciper+opinion).encode(), (MCAST_GRP, MCAST_PORT))
+
+cl_sock.close()
